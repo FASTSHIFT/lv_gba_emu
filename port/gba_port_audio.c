f12e0fd3b3c31d87ca7f46f759bcf85f3ee9b616
FASTSHIFT <vifextech@foxmail.com>
2023-08-24 01:17:07 +0800

    feat(gba_port_audio): fix pcm broken pipe

--- port/gba_port_audio.c
+++ port/gba_port_audio.c
@@ -39,18 +39,19 @@
  **********************/
 
 typedef struct {
-    volatile int16_t* buffer;
-    volatile int head;
-    volatile int tail;
+    int16_t* buffer;
+    int head;
+    int tail;
     int size;
 } audio_fifo_t;
 
 typedef struct {
+    int sample_rate;
+    audio_fifo_t fifo;
+    int16_t buffer[AUDIO_FIFO_LEN];
 #if LV_USE_SDL == 0
     snd_pcm_t* pcm_handle;
 #endif
-    audio_fifo_t fifo;
-    int16_t buffer[AUDIO_FIFO_LEN];
 } audio_ctx_t;
 
 /**********************
@@ -58,7 +59,7 @@ typedef struct {
  **********************/
 
 static size_t gba_audio_output_cb(void* user_data, const int16_t* data, size_t frames);
-static int audio_init(audio_ctx_t* ctx, int sample_rate);
+static int audio_init(audio_ctx_t* ctx);
 static void audio_fifo_init(audio_fifo_t* fifo, int16_t* buffer, int size);
 
 /**********************
@@ -81,8 +82,9 @@ int gba_audio_init(lv_obj_t* gba_emu)
 
     int sample_rate = lv_gba_emu_get_audio_sample_rate(gba_emu);
     LV_ASSERT(sample_rate > 0);
+    ctx.sample_rate = sample_rate;
 
-    ret = audio_init(&ctx, sample_rate);
+    ret = audio_init(&ctx);
     if (ret < 0) {
         return ret;
     }
@@ -152,11 +154,11 @@ static void sdl_audio_callback(void* user_data, uint8_t* stream, int len)
     }
 }
 
-static int audio_init(audio_ctx_t* ctx, int sample_rate)
+static int audio_init(audio_ctx_t* ctx)
 {
     SDL_AudioSpec audio_spec;
     memset(&audio_spec, 0, sizeof(audio_spec));
-    audio_spec.freq = sample_rate;
+    audio_spec.freq = ctx->sample_rate;
     audio_spec.format = AUDIO_S16SYS;
     audio_spec.channels = 2;
     audio_spec.samples = 1024;
@@ -174,6 +176,41 @@ static int audio_init(audio_ctx_t* ctx, int sample_rate)
 }
 
 #else
+
+static int pcm_init(audio_ctx_t* ctx)
+{
+    if (ctx->pcm_handle != NULL) {
+        snd_pcm_close(ctx->pcm_handle);
+        ctx->pcm_handle = NULL;
+    }
+
+    int ret;
+    /* Initialize ALSA PCM handle */
+    ret = snd_pcm_open(&ctx->pcm_handle, PCM_DEVICE, SND_PCM_STREAM_PLAYBACK, 0);
+    if (ret < 0) {
+        LV_LOG_ERROR("Failed to open PCM device: %s", PCM_DEVICE);
+        return ret;
+    }
+
+    LV_LOG_USER("pcm_handle = %p, sample_rate = %d", ctx->pcm_handle, ctx->sample_rate);
+    int channels = 2;
+
+    /* Set hardware parameters based on your audio file's parameters */
+    ret = snd_pcm_set_params(ctx->pcm_handle,
+        SND_PCM_FORMAT_S16_LE,
+        SND_PCM_ACCESS_RW_INTERLEAVED,
+        channels,
+        ctx->sample_rate,
+        1,
+        500000);
+    if (ret < 0) {
+        LV_LOG_ERROR("Unable to set PCM parameters: %s", snd_strerror(ret));
+        snd_pcm_close(ctx->pcm_handle);
+        ctx->pcm_handle = NULL;
+    }
+
+    return ret;
+}
 
 static void* audio_thread(void* arg)
 {
@@ -196,7 +233,10 @@ static void* audio_thread(void* arg)
 
             snd_pcm_sframes_t frames_written = snd_pcm_writei(ctx->pcm_handle, buffer, frames);
             if (frames_written < 0) {
-                LV_LOG_ERROR("Write error: %s", snd_strerror(frames_written));
+                LV_LOG_ERROR("frames = %d, Write error: %s", frames, snd_strerror(frames_written));
+
+                /* reset pcm */
+                pcm_init(ctx);
             } else if (frames_written != frames) {
                 LV_LOG_WARN("Short write, expected %d frames but wrote %ld", avaliable, frames_written);
             }
@@ -207,31 +247,10 @@ static void* audio_thread(void* arg)
     return NULL;
 }
 
-static int audio_init(audio_ctx_t* ctx, int sample_rate)
+static int audio_init(audio_ctx_t* ctx)
 {
-    int ret;
-
-    /* Initialize ALSA PCM handle */
-    ret = snd_pcm_open(&ctx->pcm_handle, PCM_DEVICE, SND_PCM_STREAM_PLAYBACK, 0);
+    int ret = pcm_init(ctx);
     if (ret < 0) {
-        LV_LOG_ERROR("Failed to open PCM device: %s", PCM_DEVICE);
-        return ret;
-    }
-
-    LV_LOG_USER("pcm_handle = %p, sample_rate = %d", ctx->pcm_handle, sample_rate);
-    int channels = 2;
-
-    /* Set hardware parameters based on your audio file's parameters */
-    ret = snd_pcm_set_params(ctx->pcm_handle,
-        SND_PCM_FORMAT_S16_LE,
-        SND_PCM_ACCESS_RW_INTERLEAVED,
-        channels,
-        sample_rate,
-        1,
-        500000);
-    if (ret < 0) {
-        LV_LOG_ERROR("Unable to set PCM parameters: %s", snd_strerror(ret));
-        snd_pcm_close(ctx->pcm_handle);
         return ret;
     }
 
